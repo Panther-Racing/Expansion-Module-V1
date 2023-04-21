eb5ccfd21dbf933df7086ba35488a1ccec07b4e6
Patrick Murphy <pam176@pitt.edu>
2023-04-21 13:47:59 -0400

    Debug code, Fixed CS
    Fixed CS and CSpin declaration.
    added I diagram test code

--- Arduino Code/TestCode/Idiagram/Idiagram.ino
+++ Arduino Code/TestCode/Idiagram/Idiagram.ino
@@ -4,19 +4,8 @@
  *
  * Repository https://github.com/Panther-Racing/Expansion-Module-V1
  *
- * Goals: 
- *  Use MCP25625(SPI to CAN) to take analog sensor readings and put data onto the CAN bus for logging purposes.
- * 
- * To Do: 
- *  Parametric BIT shifting function for formatting data lengths for adaptability to other applications
- *  Compound value
- * 
  *
- * Program Notes:
- *  CS pin is technically variable to setup
- *  CAN bus rate is variable to Setup
- *  MCP Clock is variable to setup
- *  Sensor Wiring is variable to setup
+ * FOR: I diagram testing of the CAN bus
  *
  */
 
@@ -33,10 +22,10 @@ struct can_frame frame;
     unsigned long previousMillis = 0; 
 
   //10 bit values
-    unsigned int BrkTmp=0b0000000000;
-    unsigned int DPos=0b0000000000;
-    unsigned int Aux1=0b0000000000;
-    unsigned int Aux2=0b0000000000;
+    //unsigned int BrkTmp=0b0000000000;
+    //unsigned int DPos=0b0000000000;
+    //unsigned int Aux1=0b0000000000;
+    //unsigned int Aux2=0b0000000000;
   
   //8 bit values
     uint8_t TEC=0x00; // transmit error counters
@@ -47,29 +36,28 @@ struct can_frame frame;
     uint8_t counter=0x00; // coms loss check
 
 
-  // board Chip Select Pin for SPI to CAN chip  
-    uint8_t CS=10;
-    MCP2515 mcp2515(CS);
+  // board Chip Select Pin for SPI to CAN chip 
+    MCP2515 mcp2515(10);
  
 
 
 void setup() {
   interval = 1000/interval; //conversion from HZ to MS
   //Setting Pullups on ADC pins in use
-  pinMode(A0,INPUT_PULLUP);
-  pinMode(A1,INPUT_PULLUP);
-  pinMode(A2,INPUT_PULLUP);
-  pinMode(A3,INPUT_PULLUP);
+  //pinMode(A0,INPUT_PULLUP);
+  //pinMode(A1,INPUT_PULLUP);
+  //pinMode(A2,INPUT_PULLUP);
+  //pinMode(A3,INPUT_PULLUP);
 
 
-  frame.can_id  = 0x761; //CAN ID
+  frame.can_id  = 0x760; //CAN ID
   /*
     FR-x760
     FL-x761
     RR-x762
     RL-x763
   */
-  frame.can_dlc = 8;//Data lenght of frame
+  frame.can_dlc = 2;//Data lenght of frame
   //debug control    
   while (!Serial);
   Serial.begin(115200);
@@ -82,23 +70,18 @@ void setup() {
 
 void message1() {
   //ADC Value Aquisition
-  BrkTmp=analogRead(A0);
-  DPos=analogRead(A1);
-  Aux1=analogRead(A2);
-  Aux2=analogRead(A3);
-  byte TEC=ReadReg(0x1C); // Reading the Transmit Error Counter(TEC) from the can transciever
+ // BrkTmp=analogRead(A0);
+  //DPos=analogRead(A1);
+  //Aux1=analogRead(A2);
+  //Aux2=analogRead(A3);
+  //byte TEC=ReadReg(0x1C); // Reading the Transmit Error Counter(TEC) from the can transciever
 
 
-  frame.data[0] = (BrkTmp>>2);//byte 1
-  frame.data[1] = ((BrkTmp<<6 & 0xc0) | (DPos>>4 & 0x3F));//byte 2
-  frame.data[2] = ((DPos<<4 & 0xF0) | (Aux1>>6 & 0x0F));//byte 3
-  frame.data[3] = ((Aux1<<2 & 0xFc) | (Aux2>>8 & 0x03));//byte 4
-  frame.data[4] = Aux2;//byte 5
-  frame.data[5] = firmwareversion;
-  frame.data[6] = TEC;
-  frame.data[7] = counter; 
+  frame.data[0] = 0b01100110;
+  frame.data[1] = 0b00110000;
 
   mcp2515.sendMessage(&frame);
+  Serial.println("test");
 }
 
 
@@ -128,10 +111,10 @@ void loop()
 
 byte ReadReg (byte Reg){
   // function that reads a single byte message from a specified register, 
-  digitalWrite(CS,LOW); // send SOC
+  digitalWrite(10,LOW); // send SOC
   SPI.transfer(0b00000011); //say read to device
   SPI.transfer(Reg); //saying target register
   byte output=SPI.transfer(0x00); //reading from target register
-  digitalWrite(CS,HIGH); // send EOC
+  digitalWrite(10,HIGH); // send EOC
   return output;
 }
